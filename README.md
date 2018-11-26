338a68d470df92ffa02de5b4fb2faba057d76a89
TonyS <tonysilvasd@gmail.com>
2018-11-26 09:30:03 -0800

    add initial project files

---
+++ README.md
@@ -0,0 +1,7 @@
+#Ola :wave:
+
+##Welcome to my first Express Project and my first time using NODE!
+
+It displays parts of my San Diego Top Spots project in Node.  It also lets you navigate to the data.json folder if you wanted to see the raw data used for the Project
+
+To get started, open the app in VS code, or code editor of your choosing, and install the dependencies via the terminal by typing '$ npm install'. Next, in the terminal, type '$ node server/index.js' to start the app on the local node server and open the localhost:3000 in the browser of your choice.
--- server/app.js
+++ server/app.js
@@ -1,10 +1,33 @@
 // import files and packages up here
 
+var topSpotsData = require('./data');
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var path = require('path');
+
 
 // create your express server below
-var app;
+
+//var server = app.listen(3000, function(){
+//    console.log('Application started.  Listening on port: 3000');
+// })
 
 // add your routes and middleware below
 
+app.use(morgan('dev'));
+app.use(express.static('public'))//goes to the public folder and starts displaying the html page along with the data files
+
+
+
+app.get('/data', function(req, res){
+    res.send(topSpotsData);
+});
+
+
+
 // finally export the express application
 module.exports = app;
+
+
